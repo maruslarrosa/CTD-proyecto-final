56b6d351e459f95479a5244024350dbd53697acf
Marilina Larrosa <larrosa.marilina@gmail.com>
2022-10-20 00:48:25 +0000

    Frontend: Agrego proyecto react y estructura con Header

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer';
+import { Box } from '@mui/material';
+
+function App() {
+  return (
+    <Box>
+      <Header />
+      <Body />
+      <Footer />
+    </Box>
+  );
+}
+
+export default App;
