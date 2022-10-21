a60f9b05da1de5aeb19a661d3ca7d1bcf72c1bbd
maruslarrosa <larrosa.marilina@gmail.com>
2022-10-21 15:02:15 -0300

    FRONTEND: Agrego colores estaticos y componente Header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,21 @@
+import styles from '../styles/header.module.css'
+import { Button } from './Button'
+import logoSmall from '../assets/logoSmall.png'
+import logoLarge from '../assets/logoLarge.png'
+
+
+const Header = () => {
+    return (
+      <div className={styles.header}>
+        {/* TODO: este va cuando el breakpoint es menor a 900px
+        <img className={styles.logo} src={logoSmall} /> */}
+        <img src={logoLarge} className={styles.logo}/>
+        <div className={styles.buttonContainer}>
+          <Button text={"Crear cuenta"} label={"Botón para crear cuenta"} color={"primary"}/>
+          <Button text={"Login"} label={"Botón para iniciar sesión"} color={"primary"}/>
+        </div>
+      </div>
+    );
+}
+
+export default Header
