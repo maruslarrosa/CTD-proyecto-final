316309b1a8b4f4e0b1e52abe82fc8f44f2988ec7
maruslarrosa <larrosa.marilina@gmail.com>
2022-11-10 19:15:38 -0300

    run pipeline

--- frontend/src/components/Booking.js
+++ frontend/src/components/Booking.js
@@ -1,5 +1,11 @@
+import styles from '../styles/booking.module.css'
+import Calendar from "react-calendar";
+
 export const Booking = () => {
     return (
-        <p>Booking template</p>
-    )
+      <div className={styles.bookingContainer}>
+        <h2 className={styles.title}>Fechas disponibles</h2>
+        <Calendar />
+      </div>
+    );
 }
