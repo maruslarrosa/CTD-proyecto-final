974005065149218c56607ab67851feccc981c85a
joseent <jose.ent17@gmail.com>
2022-11-10 18:30:15 -0300

    commit

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -114,7 +114,7 @@ const product = {
         },
         {
             id: 6,
-            name: "Foto 6",
+            name: "Foto 06",
             url: "https://www.gannett-cdn.com/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg"
         }
     ]
