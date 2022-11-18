5bee73a0d8285bc02b43690175369a4bea519d48
maruslarrosa <larrosa.marilina@gmail.com>
2022-11-18 14:03:13 -0300

    Agrego template general de reserva

--- frontend/src/components/Booking.js
+++ frontend/src/components/Booking.js
@@ -1,5 +1,45 @@
+import styles from "../styles/booking.module.css"
+import { Politics, ProductHeader } from "./index"
+
+
 export const Booking = () => {
+
+    const renderPersonalData = () => {
+        return (
+            <div>
+                Personal Data
+            </div>
+        )
+    }
+
+    const renderProductDetail = () => {
+        return (
+            <div>Product Detail</div>
+        )
+    }
+
+    const renderBookingDate = () => {
+        return (
+            <div>Dates</div>
+        )
+    }
+
+    const renderArrival = () => {
+        return (
+            <div>
+                Arrival
+            </div>
+        )
+    }
+
     return (
-        <p>Booking</p>
+        <p className={styles.container}>
+            <ProductHeader categoryId={"1"} name={"Nombre del hotel"}/>
+            {renderPersonalData()}
+            {renderProductDetail()}
+            {renderBookingDate()}
+            {renderArrival()}
+            <Politics />
+        </p>
     )
 }
