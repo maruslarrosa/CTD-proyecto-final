48a07f6fe7092722601e384280241039b70dd043
maruslarrosa <larrosa.marilina@gmail.com>
2022-12-08 20:20:16 -0300

    Añado ultimo progreso front

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -7,6 +7,7 @@ import { CategoryBlock } from './CategoryBlock';
 import { CategoryContainer } from './CategoryContainer';
 import { Characteristics } from './Characteristcs';
 import { CreateAccount } from './CreateAccount';
+import { CreateProduct} from './CreateProduct';
 import { Error } from './Error';
 import { Footer } from './Footer';
 import { Hamburguer } from './Hamburguer';
@@ -39,6 +40,7 @@ export {
     CategoryContainer,
     Characteristics,
     CreateAccount,
+    CreateProduct,
     Error,
     Footer,
     Hamburguer,
