347e9cd0ac034c3a93564633a04c8ed3063144fd
MarinaRA-hub <m.ramirezand@gmail.com>
2022-12-12 23:59:27 -0300

    decodificación del token para mostrarlo en componente Header

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,27 +1,30 @@
 import styles from '../styles/user.module.css'
 import { close } from '../assets'
 import { useEffect, useState } from 'react'
+import { useJwt } from "react-jwt";
 
 export const User = ({ logout }) => {
   const [userName, setUserName] = useState(window.sessionStorage.getItem('bookingUser'))
+  const { decodedToken, isExpired } = useJwt(userName);
+  console.log(decodedToken);
 
   useEffect(() => {
     setUserName(window.sessionStorage.getItem('bookingUser'))
   }, [])
   
   const getInitials = () => {
-    let initials = ''
-    const array = userName.split(' ')
-    array.forEach(element => {
-      initials += element.charAt(0)
-    });
-    return initials
+    // let initials = ''
+    // const array = userName.split(' ')
+    // array.forEach(element => {
+    //   initials += element.charAt(0)
+    // });
+    // return initials
   }
 
   return (
     <div className={styles.userContainer}>
-      <div className={styles.initials}>{getInitials()}</div>
-      <p>Hola, {userName}</p>
+      <div className={styles.initials}>{decodedToken?.name?.charAt(0).toUpperCase() + decodedToken?.lastName?.charAt(0).toUpperCase()}</div>
+      <p>Hola, {decodedToken?.name + " " + decodedToken?.lastName}</p>
       <img onClick={logout} src={close} className={styles.icon} alt="Icono de cierre"/>
     </div>
   );
