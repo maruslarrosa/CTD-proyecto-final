d22f1848d5a7c4209e3fa66830b2e7d978269729
MarinaRA-hub <m.ramirezand@gmail.com>
2022-12-13 18:44:39 -0300

    testin reporte final

--- frontend/src/components/CreateProduct.js
+++ frontend/src/components/CreateProduct.js
@@ -33,85 +33,171 @@ export const CreateProduct = () => {
     let characteristicArray = []
     data.forEach(c => {
       characteristicArray.push({
+        id: c.id,
         name: c.name,
         iconUrl: c.iconUrl,
-        selected: false
+        checked: false
       });
     })
     setCharacteristics(characteristicArray)
   }
 
-  
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setData({
+      ...data,
+      [event.target.name]: event.target.value
+    })
+  };
 
-  // Hacer el post y manejar el comportamiento
+  const handleChangeCheckboxes = id => {
+    const copyCharacteristics = [...characteristics];
+    const modifiedCharacteristics = copyCharacteristics.map(characteristics => {
+      if (id === characteristics.id) {
+        characteristics.checked = !characteristics.checked;
+      }
+      return characteristics;
+    });
+    setCharacteristics(modifiedCharacteristics);
+  };
+
+  const [characteristicsSelected, setCharacteristicsSelected] = useState([])
+
+  function characteristicFetch() {
+    for (let i = 0; i < characteristics.length; i++) {
+      if (characteristics[i].checked) {
+        // console.log("soy true", characteristics[i].name);
+        characteristicsSelected[i] = {
+          id: parseInt(characteristics[i].id),
+        };
+      }
+    }
+    return characteristicsSelected;
+  }
+
+  const [data, setData] = useState({
+    name: '',
+    description: '',
+    availability: 'Disponible',
+    policies: '',
+    latitude: 'xxx',
+    longitude: 'yyy',
+    city_id: '',
+    category_id: '',
+    characteristicsInProducts_id: characteristicsSelected,
+    imageName: '',
+    imageURL: ''
+  });
+
+
   const handleButtonClick = (event) => {
 
-    const product = {
-      name: event.target.name.value,
-      description: event.target.description.value,
-      availability: event.target.description.value,
-      policies: event.target.policies.value,
-      latitude: 'xxx',
-      longitude: 'yyy',
+    event.preventDefault();
+
+    characteristicFetch()
+
+    const producto = {
+      name: data.name,
+      description: data.description,
+      availability: "Disponibilidad",
+      policies: data.policies,
+      longitude: "xxx",
+      latitude: "yyy",
       images: [
         {
-          name: event.target.imageName.value,
-          url: event.target.imageURL.value
+          name: data.imageName,
+          url: "asd"
         }
       ],
       city_id: {
-        id: event.target.city.value
+        id: data.city_id
       },
       category_id: {
-        id: event.target.category.value
+        id: data.category_id
+      },
+      // characteristicsInProducts_id: characteristicsSelected
+      characteristicsInProducts_id: [
+        {
+          "id": "2"
+        },
+        {
+          "id": "1"
+        },
+        {
+          "id": "3"
+        }
+      ]
+    }
+
+    console.log(producto);
+
+    fetch(
+      "http://ec2-3-140-200-1.us-east-2.compute.amazonaws.com:8080/backend/productos",
+      {
+        method: "POST",
+        body: JSON.stringify(producto),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+      }
+    )
+      .then((response) => {
+        console.log(response)
+      })
+      .then((responseData) => {
+        console.log(responseData);
+        navigate("/successProduct")
+      })
+      .catch(error => console.log(error))
+  };
+
+  const handleButtonClick2 = (event) => {
+    const producto = {
+      name: "Hotel Prueba 10",
+      description: "Conoce el mejor Hotel de la ciudad",
+      availability: "Disponibilidad",
+      policies: "políticas",
+      longitude: "xxx",
+      latitude: "yyy",
+      images: [
+        {
+          name: "Imagen 2",
+          url: "https://www.imagen-1.jpg/"
+        }
+      ],
+      city_id: {
+        id: "1"
+      },
+      category_id: {
+        id: "1"
       },
       characteristicsInProducts_id: [
         {
-          id: event.target.characteristic.value
+          id: "2"
         }
       ]
-    };
-    
-
-      event.preventDefault();
-      
-        //const formattedUser = formatUser(event.target);
-        // Esto es el cuerpo del post que tengo que copiar para crear Producto
-        fetch(
-          "http://ec2-3-140-200-1.us-east-2.compute.amazonaws.com:8080/backend/productos",
-          {
-            method: "POST",
-            // crear objeto JSON con datos de producto
-            // Usar de ejemplo la función FormatdUsser de linea 35
-           body: JSON.stringify(product),
-            headers: {
-              "Content-Type": "application/json", 
-              Authorization: `Bearer ${token}`
-            },
-          }
-        )
-        try {
-          // console.log(response)
-          navigate("/successProduct")
-       } catch(e) {
-        console.log("Hubo un error")
-        console.log(e)
-       } 
-        console.log(product)
-          // .then((response) => {
-          //   console.log(response)
-          //   navigate("/successProduct")
-      
-          // })
-          // .catch(function(error)){
-          //   console.log("Hubo un error")
-          //   console.log(error)
-          // }
-
-          // .then((responseData) => {
-          //   console.log(responseData);
-          // });
-      
+    }
+    event.preventDefault();
+    fetch(
+      "http://ec2-3-140-200-1.us-east-2.compute.amazonaws.com:8080/backend/productos",
+      {
+        method: "POST",
+        body: JSON.stringify(producto),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+        mode: 'no-cors'
+      }
+    )
+      .then((response) => {
+        console.log(response)
+      })
+      .then((responseData) => {
+        console.log(responseData);
+        navigate("/successProduct")
+      });
   };
 
   return (
@@ -132,6 +218,7 @@ export const CreateProduct = () => {
                 type="text"
                 id="name"
                 name="name"
+                onChange={handleChange}
                 //required={true}
                 aria-describedby="Nombre de la propiedad"
               />
@@ -144,8 +231,9 @@ export const CreateProduct = () => {
                 className={styles.select}
                 id="category"
                 placeholder="Categoría"
-                name="category"
-                //required
+                name="category_id"
+                onChange={handleChange}
+              //required
               >
                 {categories.map((category) => (
                   <option key={category.name} value={category.id}>{category.name}</option>
@@ -163,6 +251,7 @@ export const CreateProduct = () => {
                 type="text"
                 id="address"
                 name="address"
+                // onChange={handleChange}
                 //required={true}
                 aria-describedby="Dirección"
               />
@@ -175,8 +264,9 @@ export const CreateProduct = () => {
                 className={styles.select}
                 id="city"
                 placeholder="Ciudad"
-                name="city"
-                //required
+                name="city_id"
+                onChange={handleChange}
+              //required
               >
                 {cities.map((city) => (
                   <option key={city.name} value={city.id}>{city.name}</option>
@@ -194,6 +284,7 @@ export const CreateProduct = () => {
             type="text"
             id="description"
             name="description"
+            onChange={handleChange}
             //required={true}
             aria-describedby="Descripción"
           />
@@ -208,19 +299,25 @@ export const CreateProduct = () => {
                 <div
                   className={styles.characteristic} key={characteristic.id}
                 >
-                  <input type="checkbox" name="characteristics" value={characteristic.id}/>
-                  <label>{characteristic.name}</label>
+                  <input
+                    type="checkbox"
+                    name="characteristicsInProducts_id"
+                    checked={characteristic.checked}
+                    onChange={() => handleChangeCheckboxes(characteristic.id)}
+                    value={characteristic.id}
+                  />
+                  <label htmlFor="characteristicsInProducts_id">{characteristic.name}</label>
                   <img src={characteristic.iconUrl} className={styles.icon} />
                 </div>
               ))}
             </div>
           </div>
         </div>
-{/* Apartir de aquí empiezo a escribir */}
+        {/* Apartir de aquí empiezo a escribir */}
         {/* Bloque políticas del producto */}
-            <label className={styles.label} style={{ display: 'flex', marginBottom: '5px', marginTop: '20px', fontSize:' 20px'}}>
-              Políticas del producto
-            </label>
+        <label className={styles.label} style={{ display: 'flex', marginBottom: '5px', marginTop: '20px', fontSize: ' 20px' }}>
+          Políticas del producto
+        </label>
         <div className={styles.propertyData}>
           {/* Columna Normas */}
           <div className={styles.column}>
@@ -229,11 +326,12 @@ export const CreateProduct = () => {
                 Normas de la casa
               </label>
               <div className={styles.label}>
-                  Descripción
-                </div>
+                Descripción
+              </div>
               <input
                 className={styles.description}
                 type="text"
+                onChange={handleChange}
                 id="policies"
                 name="policies"
                 //required={true}
@@ -248,8 +346,8 @@ export const CreateProduct = () => {
                 Salud y seguridad
               </label>
               <div className={styles.label}>
-                  Descripción
-                </div>
+                Descripción
+              </div>
               <input
                 className={styles.description}
                 type="text"
@@ -265,8 +363,8 @@ export const CreateProduct = () => {
                 Política de cancelación
               </label>
               <div className={styles.label}>
-                  Descripción
-                </div>
+                Descripción
+              </div>
               <input
                 className={styles.description}
                 type="text"
@@ -278,40 +376,42 @@ export const CreateProduct = () => {
         </div>
         {/* Bloque Imagenes */}
         <div>
-          <div style= {{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '15rem'}}>
+          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '15rem' }}>
             {/* <label htmlFor="imageName" className={styles.label} style={{ display: 'flex', marginBottom: '5px', marginTop: '20px', fontSize:' 20px'}}>
               Cargar imágenes
             </label> */}
-            <h3 style={{ display: 'flex', marginBottom: '5px', marginTop: '20px', fontSize:' 20px'}}>Cargar imágenes </h3>
+            <h3 style={{ display: 'flex', marginBottom: '5px', marginTop: '20px', fontSize: ' 20px' }}>Cargar imágenes </h3>
             {/* // cargar imagen */}
             <div>
               <input
-              className={styles.input}
-              type="text"
-              id="imageName"
-              name="imageName"
-              //required={true}
-              aria-describedby="nombreImagen"
-              placeholder="Nombre imagen"
-            />
-            <input
-              className={styles.input}
-              type="text"
-              id="imageURL"
-              name="imageURL"
-              //required={true}
-              aria-describedby="URL imagen"
-              placeholder="URL de la imagen"
-            />
+                className={styles.input}
+                type="text"
+                id="imageName"
+                name="imageName"
+                //required={true}
+                aria-describedby="nombreImagen"
+                placeholder="Nombre imagen"
+                onChange={handleChange}
+              />
+              <input
+                className={styles.input}
+                type="text"
+                id="imageURL"
+                onChange={handleChange}
+                name="imageURL"
+                //required={true}
+                aria-describedby="URL imagen"
+                placeholder="URL de la imagen"
+              />
             </div>
-            
-            <div className= {styles.addImageContainer}>
+
+            <div className={styles.addImageContainer}>
               <Button text="+" label="sumarImg" color="primary" />
             </div>
-            </div>
+          </div>
         </div>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <Button text="Crear" label="Crear producto" color="third" handleButtonClick={handleButtonClick} style={ {width: '3rem'}} />
+          <Button text="Crear" label="Crear producto" color="third" handleButtonClick={handleButtonClick} style={{ width: '3rem' }} />
         </div>
 
       </form>
